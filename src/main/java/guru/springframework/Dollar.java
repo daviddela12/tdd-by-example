52e4341ff118b45fae8557ae203b69701be0ba63
ddl <ddl@novatec-gmbh.de>
2022-01-31 09:54:17 +0100

    feat: equal method to compare Dollar objects

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -1,5 +1,7 @@
 package guru.springframework;
 
+import java.util.Objects;
+
 public class Dollar {
     private Double amount;
 
@@ -17,5 +19,18 @@ public class Dollar {
 
     public void setAmount(Double amount) {
         this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dollar dollar = (Dollar) o;
+        return Objects.equals(amount, dollar.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
     }
 }
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -3,6 +3,7 @@ package guru.springframework;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
 
@@ -13,5 +14,11 @@ public class MoneyTest {
         assertEquals(10d, (double) product.getAmount());
         product = dollar.multiply(3d);
         assertEquals(15d, (double) product.getAmount());
+    }
+
+    @Test
+    void testEquality() {
+        assertEquals(new Dollar(5d), new Dollar(5d));
+        assertNotEquals(new Dollar(5d), new Dollar(3d));
     }
 }
